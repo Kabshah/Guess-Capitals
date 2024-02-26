b559ea1967f0543e13f7547234cbd191a748328f
Kabshah <161329321+Kabshah@users.noreply.github.com>
2024-02-26 21:21:56 +0500

    Add files via upload

---
+++ GUESS GAME FINAL.py
@@ -0,0 +1,28 @@
+state_capitals={"New Mexico": "Santa Fe", "Texas": "Austin", "Ohio":"Columbus", "Georgia": "Atlanta",'Utah': 'Salt Lake City',
+'Vermont': 'Montpelier','Virginia': 'Richmond','Washington': 'Olympia','West Virginia': 'Charleston','Wisconsin': 'Madison',
+'Wyoming':'Cheyenne','North Carolina': 'Raleigh','North Dakota': 'Bismarck','Ohio': 'Columbus','Oklahoma': 'Oklahoma City','Oregon': 'Salem',
+'Pennsylvania': 'Harrisburg','Rhode Island': 'Providence','South Carolina': 'Columbia','South Dakota': 'Pierre','Tennessee':' Nashville',
+'New York': 'Albany' ,'Illinois': 'Springfield','Indiana': 'Indianapolis','Iowa': 'Des Moines','Kansas': 'Topeka','Kentucky': 'Frankfort',
+'Louisiana': 'Baton Rouge','Maine': 'Augusta','Maryland': 'Annapolis','Massachusetts':'Boston','Michigan': 'Lansing','Minnesota': 'St. Paul',
+'Mississippi': 'Jackson','Missouri': 'Jefferson City','Montana': 'Helena','Nebraska': 'Lincoln','Nevada': 'Carson City',
+'New Hampshire': 'Concord','New Jersey': 'Trenton','Hawaii': 'Honolulu','Idaho': 'Boise','Alabama': 'Montgomery','Alaska': 'Juneau',
+'Arizona': 'Phoenix','Arkansas': 'Little Rock','California': 'Sacramento','Colorado': 'Denver','Connecticut': 'Hartford','Delaware': 'Dover',
+'Florida': 'Tallahassee'}
+print("\t\tGuess The Capitals Game")
+correct=0
+incorrect=0
+for key,value in state_capitals.items():
+    query=input("\nWhat is the capital of " + key.title() + "? (or 'exit' to quit):").title().strip()
+    if query == value:
+        print("Your answer is correct")
+        correct += 1
+        print("Correctly answered:", correct)
+        print("Incorrectly answered:",incorrect)
+    elif query != value:
+        print("Your answer is wrong","\nThe captial of", key.title(), "is" ,value.title())
+        incorrect = incorrect + 1
+        print("Correctly Answered:", correct)
+        print("Incorrectly Answered:",incorrect)
+    elif query == 'Exit':
+        print("Thanks for playing!")
+        break
